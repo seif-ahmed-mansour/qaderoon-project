42072a2aa8944c216a926944fb57622506188580
tokhy24 <mohamed21346587@gmail.com>
2025-01-27 16:47:13 +0200

    Revert "Added Live counter animation to statistics section"
    This reverts commit cb4b89f45bb6ad6d88e0bbdee4c7c69657119381.

--- client/src/components/home/statistics-section/StatisticsCard.jsx
+++ client/src/components/home/statistics-section/StatisticsCard.jsx
@@ -1,24 +1,9 @@
-import CountUp from "react-countup";
-import { useInView } from "react-intersection-observer";
-
-const StatisticsCard = ({ icon, number, label }) => {
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-    threshold: 0.5,
-  });
-
-  return (
-    <div
-      ref={ref}
-      className="flex flex-col items-center justify-center border-2 border-transparent rounded-xl p-4 w-50 h-50 bg-gradient-to-r from-[#da1e33] to-[#a21c34] shadow-lg hover:shadow-2xl transform hover:scale-105 transition-all duration-300"
-    >
-      <div className="text-white text-6xl mb-2">{icon}</div>
-      <p className="text-white text-3xl font-bold">
-        {inView ? <CountUp end={number} duration={2} /> : number}
-      </p>
-      <p className="text-white text-xl font-medium mt-1">{label}</p>
-    </div>
-  );
-};
+const StatisticsCard = ({ icon, number, label }) => (
+  <div className="flex flex-col items-center justify-center border-2 border-transparent rounded-xl p-4 w-50 h-50 bg-gradient-to-r from-[#da1e33] to-[#a21c34] shadow-lg hover:shadow-2xl transform hover:scale-105 transition-all duration-300">
+    <div className="text-white text-6xl mb-2">{icon}</div>
+    <p className="text-white text-3xl font-bold">{number}</p>
+    <p className="text-white text-xl font-medium mt-1">{label}</p>
+  </div>
+);
 
 export default StatisticsCard;
