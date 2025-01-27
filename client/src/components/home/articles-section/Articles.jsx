9d87df0a1b9470abd4f3a2ccb2cef0c2f23f180c
seif-ahmed-mansour <seifahmedahmed111@gmail.com>
2025-01-27 07:34:28 +0200

    fixing responsive in articles

--- client/src/components/home/articles-section/Articles.jsx
+++ client/src/components/home/articles-section/Articles.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import "./articles.css";
 import Card from "../../../utils/Card/Card";
+import Button from "../../../utils/Button";
 
 function Articles() {
   const data = [
     {
       image: "/images/articles-init-photo.png", // Add your image URL here
-      description: "مقال رأي يتناول موضوع التكنولوجيا وتأثيرها على المجتمع. يناقش كيفية التقدم التكنولوجي وتحدياته.",
+      description:
+        "مقال رأي يتناول موضوع التكنولوجيا وتأثيرها على المجتمع. يناقش كيفية التقدم التكنولوجي وتحدياته.",
     },
     {
       image: "/images/logo_with_slogan.jpg", // Add your image URL here
-      description: "مقال رأي حول التعليم عن بُعد. يتحدث عن الفوائد والعيوب المرتبطة بالتعلم عبر الإنترنت.",
+      description:
+        "مقال رأي حول التعليم عن بُعد. يتحدث عن الفوائد والعيوب المرتبطة بالتعلم عبر الإنترنت.",
     },
     {
       image: "/images/home-about-section-photo.png", // Add your image URL here
-      description: "مقال رأي عن التغير المناخي. يركز على أهمية الوعي البيئي وسبل الحد من التلوث.",
+      description:
+        "مقال رأي عن التغير المناخي. يركز على أهمية الوعي البيئي وسبل الحد من التلوث.",
     },
   ];
   return (
@@ -22,12 +26,20 @@ function Articles() {
       <div className="layer"></div>
       <div className="container mx-auto px-4 text-center article-content">
         <h1 className="text-6xl my-3 text-white">مقالات الرأي</h1>
-        <p className="text-3xl my-5 text-white">مجموعة من المقالات المحدثة باستمرار</p>
-        <div className="flex flex-wrap justify-between gap-5 m-5">
+        <p className="text-3xl my-5 text-white">
+          مجموعة من المقالات المحدثة باستمرار
+        </p>
+        <div className="flex flex-wrap justify-between gap-15 m-5 md:justify-center">
           {data.map((article, index) => (
-            <Card key={index} description={article.description} image={article.image} />
+            <Card
+              key={index}
+              description={article.description}
+              image={article.image}
+            />
           ))}
         </div>
+        <Button>عرض المزيد
+        </Button>
       </div>
     </div>
   );
--- client/src/utils/Card/Card.jsx
+++ client/src/utils/Card/Card.jsx
@@ -5,14 +5,30 @@ function Card(props) {
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm">
       <a href="#" className="overflow-hidden block">
-        <img className="rounded-t-lg card-images hover:scale-125 ease-in duration-200" src={props.image} alt="" />
+        <img
+          className="rounded-t-lg card-images hover:scale-125 ease-in duration-200"
+          src={props.image}
+          alt=""
+        />
       </a>
       <div className="p-5 text-center">
         <h1 className="mb-3 text-3xl break-words my-2">{props.description}</h1>
         <Button>
-          wow
-          <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 5h12m0 0L9 1m4 4L9 9" />
+          إقرأ المزيد
+          <svg
+            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
           </svg>
         </Button>
       </div>
