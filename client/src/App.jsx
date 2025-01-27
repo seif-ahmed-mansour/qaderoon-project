d74c419d6d0d7e12ba97f97d13592a28487369bd
AhmedOsman101 <trollingosman101@outlook.com>
2025-01-27 13:02:54 +0200

    update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import HomeLandingSlider from "./components/home/LandingSlider/HomeLandingSlider";
 import Navbar from "./components/Navbar/Navbar";
 import EgyptVision from "./components/home/EgyptVision/EgyptVision";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
