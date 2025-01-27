6aa22a6ad8fd33af3a257b2d24028ba433855c50
1amir-talaat <hsiwh2be872b@gmail.com>
2025-01-27 02:52:33 +0200

    update the button and card

--- client/src/utils/Button.jsx
+++ client/src/utils/Button.jsx
@@ -1,31 +1,15 @@
 import React from "react";
-import "./button.css";
 import { Link } from "react-router-dom";
-function Button(props) {
+
+function Button({ link, children, className = "", ...rest }) {
   return (
-    <div>
-      <Link
-        to={props.link}
-        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-primary rounded-lg hover:bg-secondary focus:ring-4 focus:outline-none transition duration-150 card-button"
-      >
-        {props.name}
-        <svg
-          className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 10"
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M1 5h12m0 0L9 1m4 4L9 9"
-          />
-        </svg>
-      </Link>
-    </div>
+    <Link
+      to={link}
+      className={`inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-primary rounded-lg hover:bg-secondary focus:ring-4 focus:outline-none transition duration-150 card-button ${className}`}
+      {...rest}
+    >
+      {children}
+    </Link>
   );
 }
 
