4f244837f9d79d9715bfd1db0d5171e52d8ed9af
tokhy24 <mohamed21346587@gmail.com>
2025-01-27 03:01:50 +0200

    Added StatisticsSection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
+import StatisticsSection from "./components/home/statistics-section/StatisticsSection";
 
 function App() {
   return (
     <>
       <Routes></Routes>
-      
+      <StatisticsSection />
     </>
   );
 }
---
+++ client/src/components/home/statistics-section/StatisticsSection.jsx
@@ -0,0 +1,35 @@
+import { MdCastForEducation } from "react-icons/md";
+import { PiVideoConference } from "react-icons/pi";
+import { RiUserCommunityFill } from "react-icons/ri";
+import StatisticsCard from "./StatisticsCard";
+
+const StatisticsSection = () => (
+  <section className="bg-gray-100 py-12">
+    <div className="text-center mb-8">
+      <h2 className="text-[#da1e33] text-2xl font-bold">الإحصائيات</h2>
+      <p className="text-gray-600 text-lg mt-2">
+        أرقام تعبر عن تميز وريادة الكيان
+      </p>
+      <div className="w-16 h-1 bg-[#a21c34] mt-2 mx-auto"></div>
+    </div>
+    <div className="flex flex-wrap justify-center gap-6">
+      <StatisticsCard
+        icon={<PiVideoConference />}
+        number="6"
+        label="المؤتمرات"
+      />
+      <StatisticsCard
+        icon={<MdCastForEducation />}
+        number="85"
+        label="الدورات"
+      />
+      <StatisticsCard
+        icon={<RiUserCommunityFill />}
+        number="1870"
+        label="أعضاء الكيان"
+      />
+    </div>
+  </section>
+);
+
+export default StatisticsSection;
