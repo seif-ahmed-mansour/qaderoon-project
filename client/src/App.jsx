2ad0c077ad3b542142916f5f3c863dba5c81f164
tokhy24 <mohamed21346587@gmail.com>
2025-01-27 03:11:27 +0200

    Return the initial code for app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import AboutSectionComponent from "./components/home/about-section/AboutSectionComponent";
 
 function App() {
   return (
     <>
       <Routes></Routes>
-      <AboutSectionComponent />
     </>
   );
 }
