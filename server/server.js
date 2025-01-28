e5eaca60f0f902504894f15aacdc11b544a60105
AhmedOsman101 <trollingosman101@outlook.com>
2025-01-28 16:14:52 +0200

    refactor: improve server startup logic
    - Move server start into DB connection
    - Improve error handling on DB failure

--- server/server.js
+++ server/server.js
@@ -1,33 +1,32 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const reportRoutes = require('./Routes/reportRoutes');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const reportRoutes = require("./Routes/reportRoutes");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 50001;
 const mongoURI = process.env.MONGO_URI;
 
-mongoose.connect(mongoURI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err.message);
-    process.exit(-1);
-  });
-
-
 // to use anything in Public
-app.use(express.static('Public'))
+app.use(express.static("Public"));
 
 // to use in front
 app.use(cors());
 
-// to access routes 
-app.use('/reports', reportRoutes);
+// to access routes
+app.use("/reports", reportRoutes);
 
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// if the database connection failed don't run the server
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err.message);
+    process.exit(-1);
+  });
