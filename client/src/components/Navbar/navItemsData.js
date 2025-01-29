e54a974b551ba32bb1adbf0bc2f0a1a4a463995f
Zeyad Hyman <147080913+ZeyadHyman@users.noreply.github.com>
2025-01-29 13:56:33 +0200

    updaing Navbar

---
+++ client/src/components/Navbar/navItemsData.js
@@ -0,0 +1,34 @@
+const navItemsData = [
+  {
+    name: "من نحن",
+    link: "/about/institution",
+    subItems: [
+      { name: "عن المؤسسة", link: "/about/institution" },
+      { name: "كلمة رئيس المؤسسة", link: "/about/speech" },
+      { name: "مجلس الأمناء", link: "/about/board" },
+      { name: "الهيئة العليا والاستشارية", link: "/about/supreme-advisory" },
+      { name: "المكاتب", link: "/about/offices" },
+      { name: "الإدارات والوحدات", link: "/about/units" },
+      { name: "المحافظات", link: "/about/governorates" },
+      { name: "شركاء النجاح", link: "/about/success-partners" },
+    ],
+  },
+  {
+    name: "خدماتنا",
+    link: "/services",
+    subItems: [
+      { name: "الاستشارات والدراسات", link: "/services/consulting" },
+      { name: "التدريب", link: "/services/training" },
+      { name: "الدورات التدريبية", link: "/services/courses" },
+      { name: "التحقق من الشهادات", link: "/services/certification" },
+      { name: "المؤتمرات", link: "/services/conferences" },
+      { name: "الرحلات والمصايف", link: "/services/trips" },
+    ],
+  },
+  { name: "الأخبار", link: "/news" },
+  { name: "المقالات", link: "/articles" },
+  { name: "تواصل معنا", link: "/contact" },
+  { name: "إنضم إلينا", link: "/join-us" },
+];
+
+export default navItemsData
