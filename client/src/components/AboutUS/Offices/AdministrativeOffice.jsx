6da948db5026f9c994d080f3c589de6ade4131d3
Zeyad Hyman <147080913+ZeyadHyman@users.noreply.github.com>
2025-01-29 22:25:50 +0200

    add المكتب الفني section

--- client/src/components/AboutUS/Offices/AdministrativeOffice.jsx
+++ client/src/components/AboutUS/Offices/AdministrativeOffice.jsx
@@ -8,12 +8,12 @@ import {
 
 const AdministrativeOffice = () => (
   <section className="mb-20">
-    <h2 className="flex space-x-2 font-extrabold text-2xl md:text-4xl pb-4 underlinee text-neutral-950 mb-8">
+    <h2 data-aos="fade-down" className="flex space-x-2 font-extrabold text-xl md:text-4xl pb-4 underlinee text-neutral-950 mb-8">
       <FaUsers className="text-primary text-4xl" />
       <span>المكتب الإداري – مؤسسة شباب قادرون</span>
     </h2>
 
-    <p className="text-lg font-bold mb-10 text-gray-700 leading-relaxed">
+    <p data-aos="fade-down" className="text-base font-bold mb-10 text-gray-700 leading-relaxed">
       المكتب الإداري هو الوحدة المسؤولة عن إدارة الشؤون الإدارية والمالية لمؤسسة
       شباب قادرون، ويعمل على توفير الدعم اللازم للأنشطة والبرامج التي تنفذها
       المؤسسة من الناحيتين الإدارية والمالية، بما يضمن سير العمل بكفاءة وفعالية.
@@ -21,7 +21,7 @@ const AdministrativeOffice = () => (
 
     {/* المهام والمسؤوليات */}
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-      <div className="bg-white shadow-lg rounded-xl p-6 border-l-8 border-primary hover:shadow-lg transition-all duration-300 transform hover:-translate-y-1">
+      <div data-aos="fade-down" className="bg-white shadow-lg rounded-xl p-6 border-l-8 border-primary hover:shadow-lg transition-all duration-300 transform hover:-translate-y-1">
         <FaMoneyBillWave className="text-primary text-5xl mb-6" />
         <h4 className="text-xl font-bold mb-4 text-gray-800">
           الإدارة المالية
@@ -35,7 +35,7 @@ const AdministrativeOffice = () => (
         </ul>
       </div>
 
-      <div className="bg-white shadow-lg rounded-xl p-6 border-l-8 border-primary hover:shadow-lg transition-all duration-300 transform hover:-translate-y-1">
+      <div data-aos="fade-down" className="bg-white shadow-lg rounded-xl p-6 border-l-8 border-primary hover:shadow-lg transition-all duration-300 transform hover:-translate-y-1">
         <FaUsers className="text-primary text-5xl mb-6" />
         <h4 className="text-xl font-bold mb-4 text-gray-800">
           الإدارة الشخصية
@@ -47,7 +47,7 @@ const AdministrativeOffice = () => (
         </ul>
       </div>
 
-      <div className="bg-white shadow-lg rounded-xl p-6 border-l-8 border-primary hover:shadow-lg transition-all duration-300 transform hover:-translate-y-1">
+      <div data-aos="fade-down" className="bg-white shadow-lg rounded-xl p-6 border-l-8 border-primary hover:shadow-lg transition-all duration-300 transform hover:-translate-y-1">
         <FaTasks className="text-primary text-5xl mb-6" />
         <h4 className="text-xl font-bold mb-4 text-gray-800">
           الشؤون الإدارية
@@ -59,7 +59,7 @@ const AdministrativeOffice = () => (
         </ul>
       </div>
 
-      <div className="bg-white shadow-lg rounded-xl p-6 border-l-8 border-primary hover:shadow-lg transition-all duration-300 transform hover:-translate-y-1">
+      <div data-aos="fade-down" className="bg-white shadow-lg rounded-xl p-6 border-l-8 border-primary hover:shadow-lg transition-all duration-300 transform hover:-translate-y-1">
         <FaRegLightbulb className="text-primary text-5xl mb-6" />
         <h4 className="text-xl font-bold mb-4 text-gray-800">
           التنظيم والتخطيط
@@ -75,12 +75,12 @@ const AdministrativeOffice = () => (
     </div>
 
     {/* أهداف المكتب */}
-    <h3 className="text-2xl font-extrabold text-neutral-950 mb-8 mt-8">
+    <h3 data-aos="fade-down" className="text-2xl font-extrabold text-neutral-950 mb-8 mt-8">
       أهداف المكتب الإداري
     </h3>
 
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-      <div className="bg-white p-6 shadow-md rounded-xl hover:shadow-lg transform hover:scale-105 transition-all duration-300">
+      <div data-aos="fade-down" className="bg-white p-6 shadow-md rounded-xl hover:shadow-lg transform hover:scale-105 transition-all duration-300">
         <div className="flex items-center space-x-4 mb-4">
           <FaMoneyBillWave className="text-primary text-4xl" />
           <h4 className="text-xl font-bold text-gray-800">
@@ -92,7 +92,7 @@ const AdministrativeOffice = () => (
         </p>
       </div>
 
-      <div className="bg-white p-6 shadow-md rounded-xl hover:shadow-lg transform hover:scale-105 transition-all duration-300">
+      <div data-aos="fade-down" className="bg-white p-6 shadow-md rounded-xl hover:shadow-lg transform hover:scale-105 transition-all duration-300">
         <div className="flex items-center space-x-4 mb-4">
           <FaTasks className="text-primary text-4xl" />
           <h4 className="text-xl font-bold text-gray-800">ضمان سير العمل</h4>
@@ -102,7 +102,7 @@ const AdministrativeOffice = () => (
         </p>
       </div>
 
-      <div className="bg-white p-6 shadow-md rounded-xl hover:shadow-lg transform hover:scale-105 transition-all duration-300">
+      <div data-aos="fade-down" className="bg-white p-6 shadow-md rounded-xl hover:shadow-lg transform hover:scale-105 transition-all duration-300">
         <div className="flex items-center space-x-4 mb-4">
           <FaHandsHelping className="text-primary text-4xl" />
           <h4 className="text-xl font-bold text-gray-800">
--- client/src/components/AboutUS/Offices/Offices.jsx
+++ client/src/components/AboutUS/Offices/Offices.jsx
@@ -1,13 +1,18 @@
 import AdministrativeOffice from "./AdministrativeOffice";
 import ExecutiveOffice from "./ExecutiveOffice";
+import TechnicalOffice from "./TechnicalOffice";
 
 function Offices() {
   return (
     <div className="w-full mx-auto px-4 md:px-10 lg:px-20 my-16 text-gray-900">
       {/* المكتب الإداري */}
+
       <AdministrativeOffice />
       {/* المكتب التنفيذي */}
       <ExecutiveOffice />
+
+      {/* المكتب الفني */}
+      <TechnicalOffice />
     </div>
   );
 }
