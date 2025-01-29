a4bbcf5f63867a8d8bd6877c62ff3be1916d5176
1amir-talaat <hsiwh2be872b@gmail.com>
2025-01-29 02:08:39 +0200

    update artical section

--- client/src/components/ArticleDetails/ArticleDetails.jsx
+++ client/src/components/ArticleDetails/ArticleDetails.jsx
@@ -4,13 +4,16 @@ import { useParams } from "react-router-dom";
 
 function ArticleDetails() {
   const { id } = useParams();
-  console.log(id);
-  const [article, setArticle] = useState(null);
+
+  const [article, setArticle] = useState({});
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`${import.meta.env.VITE_HOST_SERVER}reports/${id}`);
-        // console.log(response);
+        const response = await axios.get(
+          `${import.meta.env.VITE_HOST_SERVER}reports/${id}`
+        );
+
+        console.log(response);
 
         setArticle(response.data.data);
       } catch (error) {
@@ -22,18 +25,21 @@ function ArticleDetails() {
     fetchData();
   }, [id]);
 
+  console.log(article);
+
   return (
     <>
       <div className="bg-gray-100 h-20">
-        <h2 className="text-4xl p-5">
-          {article && article.reportTitle}
-        </h2>
+        <h2 className="text-4xl p-5">{article && article.reportTitle}</h2>
       </div>
       <div className="text-center flex justify-center">
-        <img src={`${import.meta.env.VITE_HOST_SERVER}imgs/`+article.reportImg} alt="" className=" h-100 object-cover as" />
+        <img
+          src={`${import.meta.env.VITE_HOST_SERVER}imgs/` + article.reportImg}
+          alt=""
+          className=" h-100 object-cover as"
+        />
       </div>
       <p className="text-2xl m-9">{article && article.reportDesc}</p>
-      {/* nubuib */}
     </>
   );
 }
