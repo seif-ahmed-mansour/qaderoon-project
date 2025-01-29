690fe3c6870108d17cf6453f9ca491e012a6a2b9
seif-ahmed-mansour <seifahmedahmed111@gmail.com>
2025-01-30 00:00:03 +0200

    fixing card responsive

--- client/src/utils/Card/Card.jsx
+++ client/src/utils/Card/Card.jsx
@@ -3,7 +3,7 @@ import "./card.css";
 
 function Card(props) {
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-xl h-full flex flex-col justify-center">
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-xl h-full flex flex-col">
       <p className="overflow-hidden block">
         <img
           className="rounded-t-lg card-images hover:scale-105 ease-in duration-200"
