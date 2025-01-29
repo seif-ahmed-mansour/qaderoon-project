90b464c02d4f6746b0801f33d4f4f2eaabf9ec04
AhmedOsman101 <trollingosman101@outlook.com>
2025-01-29 11:56:36 +0200

    refactor(articles): improve articles display
    - Remove unnecessary dummy data
    - Refactor component structure for better readability
    - Improve image loading and styling
    - Update card component for better design
    - Adjust button styling for consistency

--- client/src/utils/Card/Card.jsx
+++ client/src/utils/Card/Card.jsx
@@ -4,25 +4,24 @@ import "./card.css";
 function Card(props) {
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-xl">
-      <a href="#" className="overflow-hidden block">
+      <p className="overflow-hidden block">
         <img
           className="rounded-t-lg card-images hover:scale-105 ease-in duration-200"
           src={props.image}
           alt=""
         />
-      </a>
+      </p>
       <div className="p-5 text-center">
         <h1 className="mb-3 text-xl break-words my-2">{props.description}</h1>
         {/* Omit the link prop since the entire card is wrapped in a Link */}
-        <Button>
+        <Button className="font-semibold">
           إقرأ المزيد
           <svg
             className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
-            viewBox="0 0 14 10"
-          >
+            viewBox="0 0 14 10">
             <path
               stroke="currentColor"
               strokeLinecap="round"
