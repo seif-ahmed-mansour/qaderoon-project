e2affbe32e6178b7b980ef41ce43b55a6cd34980
AhmedOsman101 <trollingosman101@outlook.com>
2025-01-29 19:30:41 +0200

    feat(client): add article links in AllArticles page
    - Updated articles display to use Link component.
    - Replaced Card component with Link to navigate.
    - Each article now links to its individual page.

--- client/src/pages/AllArticles.jsx
+++ client/src/pages/AllArticles.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Card from "../utils/Card/Card";
+import { Link } from "react-router-dom";
 
 function AllArticles() {
   const [articles, setArticles] = useState([]);
@@ -27,16 +28,17 @@ function AllArticles() {
       <h2 className="font-extrabold text-3xl md:text-4xl pb-5 underlineR text-neutral-950 mx-auto">
         اخر <span className="text-primary relative top-1">المقالات</span>
       </h2>
-      <div className="flex flex-wrap justify-between gap-15 m-5 md:justify-center text-center cards-containerr">
+      <div className="flex flex-wrap justify-between gap-15 m-5 md:justify-center text-center cards-containerr mt-16">
         {articles &&
-          articles.map((article, index) => (
-            <Card
-              key={index}
-              description={article.reportTitle}
-              image={
-                `${import.meta.env.VITE_HOST_SERVER}imgs/` + article.reportImg
-              }
-            />
+          articles.map(({ _id, reportTitle, reportImg }) => (
+            <Link
+              key={_id}
+              to={`/article/${_id}`}>
+              <Card
+                description={reportTitle}
+                image={`${import.meta.env.VITE_HOST_SERVER}imgs/` + reportImg}
+              />
+            </Link>
           ))}
       </div>
     </div>
