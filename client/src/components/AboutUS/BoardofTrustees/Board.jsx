32d1f79de670f1809bfe541a4bfd523e144943f8
zeyad_sofy <75764257+zeyadsofy@users.noreply.github.com>
2025-01-30 00:27:04 +0200

    updating margin in whowe sections and board sections

--- client/src/components/AboutUS/BoardofTrustees/Board.jsx
+++ client/src/components/AboutUS/BoardofTrustees/Board.jsx
@@ -5,7 +5,7 @@ import { Missions } from "./Missions"
 const Board = () => {
   return (
         <>
-        <section className=" p-1 rounded-xl mx-auto mb-0 ">
+        <section className=" my-10 p-1 rounded-xl mx-auto mb-0 lg:my-20">
             <div className="container mx-auto">
                 <AboutBroad/>
                 <Formating/>
