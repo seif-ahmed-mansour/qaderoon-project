c2e0fbd652ab8483465c880887c1c45e9ad3d878
Zeyad Hyman <147080913+ZeyadHyman@users.noreply.github.com>
2025-01-29 14:33:04 +0200

    updateing footer links

--- client/src/components/Footer/FooterSection.jsx
+++ client/src/components/Footer/FooterSection.jsx
@@ -1,15 +1,19 @@
+import { Link } from "react-router-dom";
+
 const FooterSection = ({ title, links }) => (
   <div className="space-y-3">
-    <h3 className="font-bold text-gray-900 tracking-wide uppercas text-lg">{title}</h3>
+    <h3 className="font-bold text-gray-900 tracking-wide uppercas text-lg">
+      {title}
+    </h3>
     <ul className="mt-6 space-y-4 text-md">
       {links.map((link, index) => (
         <li key={index}>
-          <a
+          <Link
             rel="noopener noreferrer"
-            href={link.href}
+            to={link.href}
             className="hover:-translate-x-3 transform block transition-transform duration-200 font-medium">
             {link.label}
-          </a>
+          </Link>
         </li>
       ))}
     </ul>
