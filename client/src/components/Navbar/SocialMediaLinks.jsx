bc43ae035f0840da6fb7131be3ddc6f434a8a9b2
AhmedOsman101 <trollingosman101@outlook.com>
2025-01-30 18:31:37 +0200

    style(Navbar): update social media links & styling
    - Updated social media icons and links.
    - Improved Navbar CSS styling.
    - Used consistent color scheme.
    - Improved responsiveness.
    - Updated font styles.

--- client/src/components/Navbar/SocialMediaLinks.jsx
+++ client/src/components/Navbar/SocialMediaLinks.jsx
@@ -1,49 +1,47 @@
+import { FaPhoneAlt, FaLinkedin, FaYoutube } from "react-icons/fa";
 import {
-  FaPhoneAlt,
-  FaLinkedin,
-  FaYoutube,
-  FaWhatsapp,
-  FaInstagram,
-  FaFacebook,
-} from "react-icons/fa";
+  IoLogoFacebook as FaFacebook,
+  IoLogoWhatsapp as FaWhatsapp,
+} from "react-icons/io";
+import { AiFillInstagram as FaInstagram } from "react-icons/ai";
 import { FaXTwitter } from "react-icons/fa6";
 
 const socialMediaLinks = [
   {
-    name: "LinkedIn",
-    icon: <FaLinkedin />,
-    link: "https://www.linkedin.com/company/qaderoon492",
-    color: "hover:text-[#262e30] text-[#425154]",
+    name: "Facebook",
+    icon: <FaFacebook />,
+    link: "https://www.facebook.com/qaderoon492",
+    color: "hover:text-[#3B5999] text-stone-600",
   },
   {
     name: "Twitter",
     icon: <FaXTwitter />,
     link: "https://x.com/qaderoon492",
-    color: "hover:text-[#262e30] text-[#425154]",
-  },
-  {
-    name: "YouTube",
-    icon: <FaYoutube />,
-    link: "https://www.youtube.com/@qaderoon492",
-    color: "hover:text-[#262e30] text-[#425154]",
-  },
-  {
-    name: "WhatsApp",
-    icon: <FaWhatsapp />,
-    link: "https://api.whatsapp.com/send?phone=201000820029&app=facebook&entry_point=page_cta",
-    color: "hover:text-[#262e30] text-[#425154]",
+    color: "hover:text-black text-stone-600",
   },
   {
     name: "Instagram",
     icon: <FaInstagram />,
     link: "https://www.instagram.com/qaderoon492/",
-    color: "hover:text-[#262e30] text-[#425154]",
+    color: "hover:text-[#E1308B] text-stone-600",
   },
   {
-    name: "Facebook",
-    icon: <FaFacebook />,
-    link: "https://www.facebook.com/qaderoon492",
-    color: "hover:text-[#262e30] text-[#425154]",
+    name: "LinkedIn",
+    icon: <FaLinkedin />,
+    link: "https://www.linkedin.com/company/qaderoon492",
+    color: "hover:text-[#0077B5] text-stone-600",
+  },
+  {
+    name: "WhatsApp",
+    icon: <FaWhatsapp />,
+    link: "https://api.whatsapp.com/send?phone=201000820029&app=facebook&entry_point=page_cta",
+    color: "hover:text-[#25D366] text-stone-600",
+  },
+  {
+    name: "YouTube",
+    icon: <FaYoutube />,
+    link: "https://www.youtube.com/@qaderoon492",
+    color: "hover:text-[#DE463B] text-stone-600",
   },
 ];
 
@@ -54,16 +52,14 @@ const SocialMediaLinks = () => (
     </button>
     <div
       dir="ltr"
-      className="absolute top-full mt-4 rounded-bl-2xl rounded-br-2xl left-0 z-50 block space-y-4 bg-white max-h-0 overflow-hidden min-w-[230px] group-hover:opacity-100 group-hover:max-h-[700px] px-6 group-hover:pb-4 group-hover:pt-6 transition-all duration-500"
-    >
+      className="absolute top-full mt-4 rounded-bl-2xl rounded-br-2xl left-0 z-50 block space-y-4 bg-white max-h-0 overflow-hidden min-w-[230px] group-hover:opacity-100 group-hover:max-h-[700px] px-6 group-hover:pb-4 group-hover:pt-6 transition-all duration-500">
       {socialMediaLinks.map((social, index) => (
         <a
           key={index}
           href={social.link}
           target="_blank"
           rel="noopener noreferrer"
-          className={`flex items-center space-x-2 ${social.color}`}
-        >
+          className={`flex items-center space-x-2 ${social.color} transition-colors duration-300`}>
           {social.icon}
           <span>{social.name}</span>
         </a>
